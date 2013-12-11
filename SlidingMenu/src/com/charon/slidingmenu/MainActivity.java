22e3eaee3d7a68aed693c8e3a59da35368b05631
Charon Chui <charon.chui@gmail.com>
2013-12-11 17:42:16 +0800

    initial import

---
+++ SlidingMenu/src/com/charon/slidingmenu/MainActivity.java
@@ -0,0 +1,73 @@
+package com.charon.slidingmenu;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+
+import com.charon.slidingmenu.fragment.CenterFragment;
+import com.charon.slidingmenu.fragment.CenterFragment.OnViewPagerChangeListener;
+import com.charon.slidingmenu.fragment.LeftFragment;
+import com.charon.slidingmenu.fragment.RightFragment;
+import com.charon.slidingmenu.view.SlidingMenu;
+
+public class MainActivity extends FragmentActivity {
+	private SlidingMenu mSlidingMenu;
+	private LeftFragment mLeftFragment;
+	private RightFragment mRightFragment;
+	private CenterFragment mCenterFragment;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		findView();
+		initView();
+	}
+
+	private void findView() {
+		mSlidingMenu = (SlidingMenu) findViewById(R.id.sm);
+	}
+
+	private void initView() {
+		View leftView = View.inflate(this, R.layout.frame_left, null);
+		View rightView = View.inflate(this, R.layout.frame_right, null);
+		View centerView = View.inflate(this, R.layout.frame_center, null);
+
+		mSlidingMenu.setView(leftView, rightView, centerView, 200, 250);
+
+		FragmentTransaction transaction = getSupportFragmentManager()
+				.beginTransaction();
+		mLeftFragment = new LeftFragment();
+		transaction.replace(R.id.fl_left, mLeftFragment);
+		mRightFragment = new RightFragment();
+		transaction.replace(R.id.fl_right, mRightFragment);
+		mCenterFragment = new CenterFragment();
+		transaction.replace(R.id.fl_center, mCenterFragment);
+		transaction.commit();
+
+		mCenterFragment
+				.setOnViewPagerChangeListener(new OnViewPagerChangeListener() {
+
+					@Override
+					public void onPageChage(int position) {
+						if (mCenterFragment.isFirst()) {
+							mSlidingMenu.setWhichSideCanShow(true, false);
+						} else if (mCenterFragment.isLast()) {
+							mSlidingMenu.setWhichSideCanShow(false, true);
+						} else {
+							mSlidingMenu.setWhichSideCanShow(false, false);
+						}
+					}
+				});
+	}
+
+	public void showLeftViewToogle() {
+		mSlidingMenu.showLeftViewToogle();
+	}
+
+	public void showRightViewToogle() {
+		mSlidingMenu.showRightViewToogle();
+	}
+
+}
